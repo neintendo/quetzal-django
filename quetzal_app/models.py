589290ba5bd2dfe144eeeb85757fa2efc9e2c873
neintendo <kambashaalfredo@gmail.com>
2025-08-17 22:40:01 +0200

    created core models for the app

--- quetzal_app/models.py
+++ quetzal_app/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=150)
+    email = models.EmailField(unique=True, max_length=320)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Currency(models.Model):
+    # PSEUDOCODE JUST FOR NOW
+    pass
+class Account(models.Model):
+
+    ACCOUNT_TYPES = [
+        ("cash", "Cash"),
+        ("bank", "Bank"),
+        ("investment", "Investment"),
+        ("savings", "Savings"),
+        ("credit", "Credit Card"),
+        ("loan", "Loan"),
+        ("other", "Other"),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=True, max_length=100)
+    type = models.CharField(max_length=50, choices=ACCOUNT_TYPES)
+    currency_type = models.ForeignKey(Currency, on_delete=models.RESTRICT)
+
+class Category(models.Model):
+    CATEGORY_TYPES = [
+        ("income", "Income"),
+        ("expense", "Expense"),
+        ("transfer", "Transfer"),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    type = models.CharField(choices=CATEGORY_TYPES)
+
+class Transaction(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    description = models.TextField()
+    date = models.DateField()
+    currency_ref = models.ForeignKey(Currency, on_delete=models.RESTRICT)
