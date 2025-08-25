94eead9733d7a5d43da04ba8e1c24a12f06fc8cc
neintendo <kambashaalfredo@gmail.com>
2025-08-25 16:39:51 +0200

    fixed UserRegistrationSerializer password related error

--- quetzal_app/serializers.py
+++ quetzal_app/serializers.py
@@ -13,17 +13,27 @@ class UserSerializer(serializers.ModelSerializer):
 
 # User Registration Serializer.
 class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'main_currency']
+        fields = ['id', 'username', 'email', 'password', 'main_currency']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            main_currency=validated_data.get('main_currency', 'USD')
-        )
+        # Extract password from validated_data
+        password = validated_data.pop('password', None)
+
+        # Create user without password first
+        user = User.objects.create(**validated_data)
+
+        # Set password using set_password method (hashes the pass)
+        if password:
+            user.set_password(password)
+            user.save()
+
         return user
 
 # User Login Serializer.
