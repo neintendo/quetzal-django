395a887668e8ef7e6ad4dc7e04c4e1d373d4755f
neintendo <kambashaalfredo@gmail.com>
2025-10-02 18:55:27 +0200

    secret key stuff :)

--- quetzal/settings.py
+++ quetzal/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#f2b+zxib73j=u3op$((&-5&q7x8p#^^jb$8@zmf5$m^n-lxo='
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,73 +34,71 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'quetzal_app',
-    'django_filters',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "quetzal_app",
+    "django_filters",
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
-    'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
-    ]
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
-AUTH_USER_MODEL = 'quetzal_app.User'
+AUTH_USER_MODEL = "quetzal_app.User"
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'quetzal.urls'
+ROOT_URLCONF = "quetzal.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'quetzal.wsgi.application'
+WSGI_APPLICATION = "quetzal.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quetzal',
-        'USER': 'neintendo',
-        'PASSWORD': 'quetzal-pass',
-        'HOST': 'localhost',
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "quetzal",
+        "USER": "neintendo",
+        "PASSWORD": "quetzal-pass",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
@@ -107,16 +108,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -124,9 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -136,9 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- quetzal_app/views.py
+++ quetzal_app/views.py
@@ -1,15 +1,19 @@
-from unicodedata import category
-
 from rest_framework import generics, permissions, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Account, Category, Transaction
-from .serializers import UserSerializer, AccountSerializer, CategorySerializer, TransactionSerializer
+from .serializers import (
+    UserSerializer,
+    AccountSerializer,
+    CategorySerializer,
+    TransactionSerializer,
+)
 from django.db import transaction as db_transaction
 import django_filters
 from django_filters.rest_framework import DjangoFilterBackend
 from django.db.models import Q, Sum
+
 
 # Users
 class UserProfileView(APIView):
@@ -26,6 +30,7 @@ class UserProfileView(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 # Accounts
 class AccountsListCreateView(generics.ListCreateAPIView):
     serializer_class = AccountSerializer
@@ -37,12 +42,14 @@ class AccountsListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+
 class AccountDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AccountSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Account.objects.filter(user=self.request.user)
+
 
 # Categories
 class CategoriesListCreateView(generics.ListCreateAPIView):
@@ -55,6 +62,7 @@ class CategoriesListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+
 class CategoriesDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -62,22 +70,27 @@ class CategoriesDetailView(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         return Category.objects.filter(user=self.request.user)
 
+
 # Class for transaction filtering.
 class TransactionFilter(django_filters.FilterSet):
-    start_date = django_filters.DateFilter(field_name='datetime', lookup_expr='gte')
-    end_date = django_filters.DateFilter(field_name='datetime', lookup_expr='lte')
-    account = django_filters.CharFilter(method='filter_account')
-    category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
+    start_date = django_filters.DateFilter(field_name="datetime", lookup_expr="gte")
+    end_date = django_filters.DateFilter(field_name="datetime", lookup_expr="lte")
+    account = django_filters.CharFilter(method="filter_account")
+    category = django_filters.CharFilter(
+        field_name="category__name", lookup_expr="icontains"
+    )
 
     class Meta:
         model = Transaction
-        fields = ['transaction_type']
+        fields = ["transaction_type"]
 
     def filter_account(self, queryset, value):
         return queryset.filter(
-            Q(account__name__icontains=value) |
-            Q(destination_account__name__icontains=value)
+            Q(account__name__icontains=value)
+            | Q(destination_account__name__icontains=value)
         )
+
+
 # Transactions
 class TransactionListCreateView(generics.ListCreateAPIView):
     serializer_class = TransactionSerializer
@@ -86,12 +99,12 @@ class TransactionListCreateView(generics.ListCreateAPIView):
     filterset_class = TransactionFilter
 
     def get_queryset(self):
-        return Transaction.objects.filter(user=self.request.user).order_by('datetime')
+        return Transaction.objects.filter(user=self.request.user).order_by("datetime")
 
     def get_serializer_context(self):
         # Pass the request to the serializer context
         context = super().get_serializer_context()
-        context['request'] = self.request
+        context["request"] = self.request
         return context
 
     @db_transaction.atomic
@@ -101,19 +114,25 @@ class TransactionListCreateView(generics.ListCreateAPIView):
 
         # Updates the account balance.
         account = transaction.account
-        if transaction.transaction_type == 'income':
+        if transaction.transaction_type == "income":
             account.balance += transaction.amount
             account.save()
-        elif transaction.transaction_type == 'expense':
+        elif transaction.transaction_type == "expense":
             account.balance -= transaction.amount
-            account.save()            
-        elif transaction.transaction_type == 'transfer':
+            account.save()
+        elif transaction.transaction_type == "transfer":
+            # Checks if currencies are different then converts & deducts from the origin account and adds to destination account
+            if transaction.account.currency != transaction.destination_account.currency:
+                print("Cannot transfer between accounts with different currencies!")
+                return None
+
             # Deducts from the origin account and adds to destination account
-            if transaction.account and transaction.destination_account:
+            elif transaction.account and transaction.destination_account:
                 transaction.account.balance -= transaction.amount
                 transaction.destination_account.balance += transaction.amount
                 transaction.account.save()
                 transaction.destination_account.save()
+
 
 class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TransactionSerializer
@@ -130,11 +149,11 @@ class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
 
         # Reverts the old transaction's effect
         account = old_transaction.account
-        if old_category_type == 'income':
+        if old_category_type == "income":
             account.balance -= old_amount
-        elif old_category_type == 'expense':
+        elif old_category_type == "expense":
             account.balance += old_amount
-        elif old_category_type == 'transfer':
+        elif old_category_type == "transfer":
             old_transaction.account.balance += old_amount
             old_transaction.destination_account.balance -= old_amount
             old_transaction.account.save()
@@ -147,13 +166,13 @@ class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
         new_amount = updated_transaction.amount
 
         # Apply the new transaction's effect
-        if new_type == 'income':
+        if new_type == "income":
             updated_transaction.account.balance += new_amount
             updated_transaction.account.save()
-        elif new_type == 'expense':
+        elif new_type == "expense":
             updated_transaction.account.balance -= new_amount
             updated_transaction.account.save()
-        elif new_type == 'transfer':
+        elif new_type == "transfer":
             updated_transaction.account.balance -= new_amount
             updated_transaction.destination_account.balance += new_amount
             updated_transaction.account.save()
@@ -165,17 +184,18 @@ class TransactionDetailView(generics.RetrieveUpdateDestroyAPIView):
         # Revert the transaction's effect on account balance
         account = instance.account
 
-        if instance.transaction_type == 'income':
+        if instance.transaction_type == "income":
             account.balance -= instance.amount
-        elif instance.transaction_type == 'expense':
+        elif instance.transaction_type == "expense":
             account.balance += instance.amount
-        elif instance.transaction_type == 'transfer':
+        elif instance.transaction_type == "transfer":
             instance.account.balance += instance.amount
             instance.destination_account.balance -= instance.amount
             instance.account.save()
             instance.destination_account.save()
         account.save()
         instance.delete()
+
 
 class TransactionAggregateView(APIView):
     permission_classes = [IsAuthenticated]
@@ -185,40 +205,52 @@ class TransactionAggregateView(APIView):
         transactions = Transaction.objects.filter(user=request.user)
 
         # Filtered transactions.
-        transactions_type = request.GET.get('transaction_type')
+        transactions_type = request.GET.get("transaction_type")
         if transactions_type:
             transactions = transactions.filter(transaction_type=transactions_type)
 
-        category = request.GET.get('category')
+        category = request.GET.get("category")
         if category:
             transactions = transactions.filter(category__name__icontains=category)
 
-        account = request.GET.get('account')
+        account = request.GET.get("account")
         if account:
             transactions = transactions.filter(
-                Q(account__name__icontains=account) |
-                Q(destination_account__name__icontains=account)
+                Q(account__name__icontains=account)
+                | Q(destination_account__name__icontains=account)
             )
 
-        start_date = request.GET.get('start_date')
+        start_date = request.GET.get("start_date")
         if start_date:
             transactions = transactions.filter(datetime__date__gte=start_date)
 
-        end_date = request.GET.get('end_date')
+        end_date = request.GET.get("end_date")
         if end_date:
             transactions = transactions.filter(datetime__date__lte=end_date)
 
-        income_expense_transactions = transactions.exclude(transaction_type='transfer')
+        income_expense_transactions = transactions.exclude(transaction_type="transfer")
 
         # Calculate expenses, income & net worth (EXCLUDING transfers)
-        income = income_expense_transactions.filter(transaction_type='income').aggregate(total=Sum('amount'))['total'] or 0
-        expenses = income_expense_transactions.filter(transaction_type='expense').aggregate(total=Sum('amount'))['total'] or 0
+        income = (
+            income_expense_transactions.filter(transaction_type="income").aggregate(
+                total=Sum("amount")
+            )["total"]
+            or 0
+        )
+        expenses = (
+            income_expense_transactions.filter(transaction_type="expense").aggregate(
+                total=Sum("amount")
+            )["total"]
+            or 0
+        )
         net = income - expenses
 
-        return Response({
-            'income': float(income),
-            'expense': float(expenses),
-            'net': float(net),
-            'transaction_count': transactions.count(),
-            'filters_applied': dict(request.GET)
-        })
+        return Response(
+            {
+                "income": float(income),
+                "expense": float(expenses),
+                "net": float(net),
+                "transaction_count": transactions.count(),
+                "filters_applied": dict(request.GET),
+            }
+        )
