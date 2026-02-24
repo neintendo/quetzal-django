6f151d26053c51a6c4cdafb4717b66135dbe5699
neintendo <kambashaalfredo@gmail.com>
2026-02-25 01:19:59 +0200

    added styling & paths for Login & Register pages

--- quetzal-frontend/src/pages/NotFound.jsx
+++ quetzal-frontend/src/pages/NotFound.jsx
@@ -1,5 +1,10 @@
 function NotFound() {
-  return <div>Not found</div>;
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>The page you're looking for does not exist!</p>
+    </div>
+  );
 }
 
 export default NotFound;
--- quetzal-frontend/src/pages/Register.jsx
+++ quetzal-frontend/src/pages/Register.jsx
@@ -1,5 +1,7 @@
+import Form from "../components/Form";
+
 function Register() {
-  return <div>Register</div>;
+  return <Form route="/auth/register/" method="register" />;
 }
 
 export default Register;
