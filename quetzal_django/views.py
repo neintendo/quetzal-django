96877f37226c60bc64ad823a52b28e7d32644f47
neintendo <kambashaalfredo@gmail.com>
2026-03-24 13:53:52 +0200

    views.py: `AccountsGraphView` adapts currency to filter selected
    The backend now only converts currencies to `main_currency` if a filter
    is selected.

--- quetzal_django/views.py
+++ quetzal_django/views.py
@@ -319,6 +319,12 @@ class AccountsGraphView(APIView):
         transactions = (Transaction.objects.filter(user=request.user)).exclude(
             transaction_type="transfer"
         )
+        # Filters request by currency
+        currency = request.GET.get("currency")
+        print("Currency:", currency)
+        if currency:
+            transactions = transactions.filter(currency=currency)
+
         # User's main currency
         main_currency = getattr(request.user, "main_currency", "USD")
 
@@ -330,7 +336,12 @@ class AccountsGraphView(APIView):
             month_key = transaction.datetime.strftime("%Y-%m")
             amount = transaction.amount
 
-            if transaction.currency != main_currency and transaction.amount != 0:
+            # For aggregate conversions
+            if (
+                transaction.currency != main_currency
+                and transaction.amount != 0
+                and currency is None
+            ):
                 converted_amount = conversion(
                     transaction.amount,
                     transaction.currency,
