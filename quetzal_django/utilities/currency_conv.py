b7e9ed99428c70ac05f2e7f923c83b97c3f4f11a
neintendo <kambashaalfredo@gmail.com>
2026-03-24 21:20:12 +0200

    feat: improve cache-first lookup for currency rates.
    Rewrote the conversion (currency_conv.py) function to store currency
    rates for dates & currencies found in the transactions or accounts data
    to the PostgreSQL database. This improves the graph's loading speeds &
    reduces API calls.

--- quetzal_django/utilities/currency_conv.py
+++ quetzal_django/utilities/currency_conv.py
@@ -1,61 +1,61 @@
 from datetime import datetime
 
-from .frankfurter_api import cache_rates, ff_historical_func, ff_today_func
+import requests
+
+from ..models import ExchangeRates
 
 
 def conversion(amount, base_currency, target_currency, transaction_date):
 
     today = datetime.today().strftime("%Y-%m-%d")
-    print(today)
-    base_path = (
-        "quetzal_django/utilities/frankfurter_cache/" + base_currency.lower() + ".json"
-    )
+
     if transaction_date.strftime("%Y-%m-%d") > today:
         raise ValueError(
             "Date of transfers cannot be in the future between accounts with different currencies"
         )
 
-    # Today's rates conversion
-    if today == transaction_date.strftime("%Y-%m-%d"):
+    if ExchangeRates.objects.filter(date=transaction_date, base=base_currency).exists():
+        print("{0} for {1} EXISTS".format(base_currency, transaction_date))
+        rates_obj = ExchangeRates.objects.get(date=transaction_date, base=base_currency)
+        rates = rates_obj.rates
+        multiplier = rates.get(target_currency)
+
+    else:
         try:
-            with open(
-                "quetzal_django/utilities/frankfurter_cache/date_tracker.txt", "r"
-            ) as date_cmp:
-                # Downloads and caches new rates if date_tracker.txt is older than today()
-                if today != date_cmp.read():
-                    cache_rates("quetzal_django/utilities/frankfurter_cache/")
-                    response = ff_today_func(base_path)
-                else:
-                    response = ff_today_func(base_path)
+            print("{0} for {1} DOES NOT EXIST".format(base_currency, transaction_date))
+            url = (
+                "https://api.frankfurter.dev/v1/"
+                + transaction_date.strftime("%Y-%m-%d")
+                + "?base="
+                + base_currency.upper()
+            )
+            response = requests.get(url)
 
-        except Exception as read_err:
-            print("FILE ERROR", read_err)
-            return
+            if response.status_code != 200:
+                print("API Returned Error:", response.status_code)
+                return
 
-    # Historical rates conversion
-    elif today != transaction_date.strftime("%Y-%m-%d"):
-        response = ff_historical_func(transaction_date, base_currency)
-
-    data_type = type(response).__name__  # Get type name as string
-
-    match data_type:
-        case "dict":
-            # Cached request
-            data = response
-        case "Response":
-            # Parsing .json the request to dictionary
             data = response.json()
+            rates_n = data.get("rates", {})
 
-    # Access exchange rate multiplier for the target currency based on base_currency
-    multiplier = data["rates"][target_currency.upper()]
+            try:
+                ExchangeRates.objects.create(
+                    date=transaction_date, base=base_currency, rates=rates_n
+                )
+                print(
+                    "{0} for {1} CREATE SUCCESS".format(base_currency, transaction_date)
+                )
+            except Exception as e:
+                print("ERROR:", e)
+                return
+
+            multiplier = rates_n[target_currency.upper()]
+
+        except Exception as e:
+            print("Error: {0}".format(e))
+            return
 
     # Convert the currency
     amount = float(amount)
     amount *= multiplier
     return amount
-
-
-# DEVELOPMENT
-
-# transaction_dates = datetime.today()
-# print(conversion(4539, "zar", "usd", transaction_dates))
