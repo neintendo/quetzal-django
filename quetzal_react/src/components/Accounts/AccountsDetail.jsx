88c654d02843b2f725d5ad7667f430c2309aa816
neintendo <kambashaalfredo@gmail.com>
2026-04-17 03:36:07 +0200

    refactor (AccountsDetail): remove `destination_account` table entry

--- quetzal_react/src/components/Accounts/AccountsDetail.jsx
+++ quetzal_react/src/components/Accounts/AccountsDetail.jsx
@@ -51,10 +51,7 @@ const AccountsDetail = ({
     let aValue = a[sortHeader.key];
     let bValue = b[sortHeader.key];
 
-    if (
-      sortHeader.key === "amount" ||
-      sortHeader.key === "destination_account"
-    ) {
+    if (sortHeader.key === "amount") {
       if (sortHeader.key === "amount") {
         aValue = parseFloat(aValue);
         a.transaction_type === "expense" ? (aValue *= -1) : aValue;
@@ -125,14 +122,6 @@ const AccountsDetail = ({
                   : "↓"
                 : ""}
             </th>
-            <th onClick={() => requestSort("destination_account")}>
-              Dest. Account{" "}
-              {sortHeader.key === "destination_account"
-                ? sortHeader.direction === "asc"
-                  ? "↑"
-                  : "↓"
-                : ""}
-            </th>
           </tr>
           {sortedData.map((val, key) => {
             return (
@@ -169,7 +158,6 @@ const AccountsDetail = ({
                 >
                   {val.transaction_type}
                 </td>
-                <td style={{ width: 150 }}>{val.destination_account}</td>
               </tr>
             );
           })}
