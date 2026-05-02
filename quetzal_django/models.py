29eb9ec90cfab701ead55da9c93162e2c9c4a78e
neintendo <kambashaalfredo@gmail.com>
2026-05-02 04:37:42 +0200

    fix (Models): add `null` & `blank` fields to notes in Transaction model

--- quetzal_django/models.py
+++ quetzal_django/models.py
@@ -266,7 +266,7 @@ class Transaction(models.Model):
     )
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     description = models.TextField()
-    notes = models.TextField()
+    notes = models.TextField(null=True, blank=True)
     datetime = models.DateTimeField(db_default=Now())
     currency = models.CharField(
         max_length=30, choices=ExchangeRates.CURRENCIES, default="USD"
