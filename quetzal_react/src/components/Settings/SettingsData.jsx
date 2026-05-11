60167157f69e977a6612d9db89a476d55c62f971
neintendo <kambashaalfredo@gmail.com>
2026-05-11 12:40:28 +0200

    feat: export user transactions as CSV

--- quetzal_react/src/components/Settings/SettingsData.jsx
+++ quetzal_react/src/components/Settings/SettingsData.jsx
@@ -1,6 +1,33 @@
 import "../../styles/Settings/SettingsContent.css";
+import api from "../../api";
+import { useEffect, useState } from "react";
 
 const SettingsData = () => {
+  const [transactionsData, setTransactionsData] = useState("");
+
+  useEffect(() => {
+    const getCSV = () => {
+      api
+        .get("transactions/export/")
+        .then((res) => res.data)
+        .then((data) => {
+          setTransactionsData(data);
+        })
+        .catch((err) => alert(err));
+    };
+
+    getCSV();
+  }, []);
+
+  const exportCSV = () => {
+    const element = document.createElement("a");
+    const file = new Blob([transactionsData], { type: "text/csv" });
+    element.href = URL.createObjectURL(file);
+    element.download = "quetzal_export.csv";
+    document.body.appendChild(element);
+    element.click();
+  };
+
   return (
     <form className="settings-content-container">
       <div className="settings-sub-content-container">
@@ -11,7 +38,14 @@ const SettingsData = () => {
       </div>
       <div className="settings-sub-content-container">
         <div className="settings-content-header">Export All Transactions</div>
-        <button className="data-settings-content-button" type="button">
+        <div className="warning-text">
+          File may contain sensitive data. Delete after use!
+        </div>
+        <button
+          className="data-settings-content-button"
+          type="button"
+          onClick={exportCSV}
+        >
           {"Export to CSV"}
         </button>
       </div>
