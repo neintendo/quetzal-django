8320d6ffe235a688be61c9a3d2dc4566cc8bc4e3
neintendo <kambashaalfredo@gmail.com>
2026-05-27 16:49:23 +0200

    dev: switch database to SQLite

--- quetzal/settings.py
+++ quetzal/settings.py
@@ -98,12 +98,8 @@ WSGI_APPLICATION = "quetzal.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "quetzal",
-        "USER": "neintendo",
-        "PASSWORD": "quetzal-pass",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "quetzal.sqlite3",
     }
 }
 
