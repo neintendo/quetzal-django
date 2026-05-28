881d65dc04e858fe1306727659e3e54f96135a6f
neintendo <kambashaalfredo@gmail.com>
2026-05-28 05:19:20 +0200

    fix [RecentTransactions]: show "No Data" when transactions are empty

--- quetzal_react/src/components/Dashboard/RecentTransactions.jsx
+++ quetzal_react/src/components/Dashboard/RecentTransactions.jsx
@@ -18,41 +18,47 @@ const RecentTransactions = () => {
   }, []);
 
   return (
-    <table className={styles.table}>
-      <tbody className={styles.tbody}>
-        <tr className={styles.tr}>
-          <th className={styles.th}>Description</th>
-          <th className={styles.th}>Amount</th>
-          <th className={styles.th}>Category</th>
-        </tr>
-        {transactionsData.map((val, key) => {
-          return (
-            <tr
-              className={styles.tr}
-              // onClick={() =>
-              //   onRowClick(
-              //     val.id,
-              //     val.datetime,
-              //     val.description,
-              //     val.notes,
-              //     val.amount,
-              //     val.category,
-              //     val.account,
-              //     val.currency,
-              //     val.transaction_type,
-              //     val.linked_transaction,
-              //   )
-              // }
-              key={key}
-            >
-              <td className={styles.td}>{val.description}</td>
-              <td className={styles.td}>{val.amount}</td>
-              <td className={styles.td}>{val.category}</td>
+    <>
+      {transactionsData.length === 0 ? (
+        <div className={styles["table-loading"]}>[ No Data ]</div>
+      ) : (
+        <table className={styles.table}>
+          <tbody className={styles.tbody}>
+            <tr className={styles.tr}>
+              <th className={styles.th}>Description</th>
+              <th className={styles.th}>Amount</th>
+              <th className={styles.th}>Category</th>
             </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            {transactionsData.map((val, key) => {
+              return (
+                <tr
+                  className={styles.tr}
+                  // onClick={() =>
+                  //   onRowClick(
+                  //     val.id,
+                  //     val.datetime,
+                  //     val.description,
+                  //     val.notes,
+                  //     val.amount,
+                  //     val.category,
+                  //     val.account,
+                  //     val.currency,
+                  //     val.transaction_type,
+                  //     val.linked_transaction,
+                  //   )
+                  // }
+                  key={key}
+                >
+                  <td className={styles.td}>{val.description}</td>
+                  <td className={styles.td}>{val.amount}</td>
+                  <td className={styles.td}>{val.category}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
 
